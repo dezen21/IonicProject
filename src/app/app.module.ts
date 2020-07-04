acc11c4359d0a7325389e5cddd1a052855028569
dezen <dezen21@gmail.com>
2020-07-04 23:46:52 +0700

    ionicProject

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { MediaCapture } from '@ionic-native/media-capture/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { VideoEditor } from '@ionic-native/video-editor/ngx';
+import { StreamingMedia } from '@ionic-native/streaming-media/ngx';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    MediaCapture,
+    File,
+    VideoEditor,
+    StreamingMedia,
+    WebView
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
